58d4e595c415b6d419fdefbdf764471161511bf7
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2026-05-01 19:16:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -35,17 +35,70 @@ args = parser.parse_args()
 source = Path(args.source)
 destination = Path(args.destination)
 
-if not source.exists() or not source.is_dir():
-    print("Source folder does not exist or is not a directory")
-    exit()
 
-destination.mkdir(parents=True, exist_ok=True)
+# def read_folder(source):
+#     if not source.exists() or not source.is_dir():
+#         print("Source folder does not exist or is not a directory")
+#         exit()
 
-for path in source.rglob("*"):
-    if path.is_file():
-        folder_name = path.suffix[1:] if path.suffix else "no_extension"
+#     result = []  # список
+
+#     for path in source.rglob("*"):
+#         if path.is_file():
+#             folder_name = path.suffix[1:] if path.suffix else "no_extension"
+#             result.append(folder_name)  
+
+#     return result
+
+def read_folder(source):
+    if not source.exists() or not source.is_dir():
+        print("Source folder does not exist or is not a directory")
+        exit()
+
+    result = []
+
+    for path in source.rglob("*"):
+        if path.is_file():
+            folder_name = path.suffix[1:] if path.suffix else "no_extension"
+            result.append((path, folder_name))  # 🔥 кортеж
+
+    return result
+
+# def create_folder(destination, result):
+#     created_dirs = []
+#     for folder_name in result:
+#         target_dir = destination / folder_name
+#         target_dir.mkdir(parents=True, exist_ok=True)
+#         created_dirs.append(target_dir)
+#     return created_dirs
+
+def create_folder(destination, result):
+    created_dirs = {}
+
+    for _, folder_name in result:
         target_dir = destination / folder_name
         target_dir.mkdir(parents=True, exist_ok=True)
+        created_dirs[folder_name] = target_dir
 
-        shutil.copy2(path, target_dir / path.name)
-        print(f"Copied: {path.name} -> {target_dir}")
+    return created_dirs
+
+# def copy_file(target_dir, path):
+#     shutil.copy2(path, target_dir / path.name)
+#     print(f"Copied: {path.name} -> {target_dir}")
+
+# def copy_file(files, folders):
+#     for path, folder_name in files:
+#         target_dir = folders[folder_name]
+#         shutil.copy2(path, target_dir / path.name)
+#         print(f"Copied: {path.name} -> {target_dir}")
+
+
+# if __name__ == "__main__":
+#     folder_names = read_folder(source)
+#     create_folder(destination, folder_names)
+#     # copy_file(source_folder, destination)   
+
+if __name__ == "__main__":
+    files = read_folder(source)
+    folders = create_folder(destination, files)
+    # copy_file(files, folders)
